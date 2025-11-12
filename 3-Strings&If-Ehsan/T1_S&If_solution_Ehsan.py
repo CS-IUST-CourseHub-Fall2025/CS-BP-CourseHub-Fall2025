a63f327305dba179f94b64837ce438a2cba6a928
Ehsan101Herosan <ehsan.moeini.stream@gmail.com>
2025-11-12 05:50:49 -0700

    Update T1_S&If_solution_Ehsan.py

--- 3-Strings&If-Ehsan/T1_S&If_solution_Ehsan.py
+++ 3-Strings&If-Ehsan/T1_S&If_solution_Ehsan.py
@@ -1,5 +1,4 @@
-before = input()
-after = input()
+before, after = input().split()
 
 if (len(after) != len(before)):
     print("IMPOSSIBLE")
@@ -10,16 +9,16 @@ b1 = before.find('<')
 f2 = after.find('>')
 b2 = after.find('<')
 
-if (f2 < f1):
+if (f1 != -1 and f2 != -1 and f2 < f1):
     print("IMPOSSIBLE")
     exit()
-if (b2 > b1):
-    print("IMPOSSIBLE")
-    exit()
-    
 
-if ((f1 != -1) and (b1 != -1)):
-    if ((f1 < b1) and (f2 > b2)):
+if (b1 != -1 and b2 != -1 and b2 > b1):
+    print("IMPOSSIBLE")
+    exit()
+
+if (f1 != -1 and b1 != -1 and f2 != -1 and b2 != -1):
+    if (f1 < b1 and f2 > b2):
         print("IMPOSSIBLE")
         exit()
 

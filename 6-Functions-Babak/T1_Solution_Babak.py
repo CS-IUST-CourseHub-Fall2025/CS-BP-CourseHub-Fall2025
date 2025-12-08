d1243d010b0bb0096ef078cca86a094d79af0c29
Babak Nezamzadeh <80030889+BabakNZ@users.noreply.github.com>
2025-12-09 01:01:41 +0330

    Add max_gold function for gold calculation
    Implement recursive function to calculate maximum gold from caves using memoization.

---
+++ 6-Functions-Babak/T1_Solution_Babak.py
@@ -0,0 +1,31 @@
+#did you know that dp problems are really nice for our bp students? bet you didn't know
+def max_gold(caves, n, memo=None):
+    """
+    تابع بازگشتی برای محاسبه بیشترین مقدار طلا
+    caves: لیست مقادیر طلا در هر غار
+    n: ایندکس فعلی غار
+    memo: دیکشنری برای ذخیره‌سازی نتایج (memoization)
+    """
+    if memo is None:
+        memo = {}
+    
+    if n < 0:
+        return 0
+    
+    if n in memo:
+        return memo[n]
+    
+    take_current = caves[n] + max_gold(caves, n - 2, memo)
+    
+    skip_current = max_gold(caves, n - 1, memo)
+    
+    memo[n] = max(take_current, skip_current)
+    return memo[n]
+
+
+n = int(input())
+caves = list(map(int, input().split()))
+
+# محاسبه و چاپ نتیجه
+result = max_gold(caves, n - 1)
+print(result)

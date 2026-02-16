1b199afc173e0bde8c5a34df25fc95b2caab9fa3
Negar Rezaei <negar.rezaei.r@gmail.com>
2026-02-16 22:24:13 +0330

    Add Sina's strings and conditionals task

---
+++ 3-Strings&If-Sina/T1_S&I_solution_Sina.py
@@ -0,0 +1,16 @@
+s = input()
+s = s.lower()
+
+if len(s) % 2 == 1:
+    mid = len(s) // 2
+    if s[0] == s[mid] == s[-1]:
+        print("yes")
+    else:
+        print("no")
+else:
+    mid1 = len(s) // 2 - 1
+    mid2 = len(s) // 2
+    if s[0] == s[mid1] == s[mid2] == s[-1]:
+        print("yes")
+    else:
+        print("no")
